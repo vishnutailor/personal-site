1bdc007c76c7274583d9b302dee10632de4f83ca
vishnutailor <30vishnu.tailor@gmail.com>
2025-11-19 22:49:00 -0700

    Add files via upload
    Initial site - pilot

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const yearSpan = document.getElementById("year");
+  if (yearSpan) {
+    yearSpan.textContent = new Date().getFullYear();
+  }
+});
